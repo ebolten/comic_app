75fc26a3b9c3438fd36b9070a125f0223d297d07
Emily Bolten <emilybolten@gmail.com>
2019-07-08 16:33:31 -0400

    index page is seeded

--- comic_frontend/src/index.js
+++ comic_frontend/src/index.js
@@ -1,5 +1,9 @@
 
-const allComicsDiv = document.getElementById('comicDiv');
+// get all info from marvel comic api and store it locally
+const ts = Date.now()
+const privateKey = `f5952eba34e221888fc1781c728c129bc0b2f258`
+const publicKey = `ac5ddc00ec3a557a8ca5ba50cb6f6dad`
+const hash = `6d2d52ca118da80bf099aa9f227845a0`
 
 //TO DO HERE
 /*Create a function to render the index page:
@@ -12,31 +16,47 @@ addEventListener('DOMContentLoaded',allComics);
 
 //render all comics in get request
 function allComics(event) {
+    const allComicsDiv = document.getElementById('comicDiv');
 
-    fetch('https://localhost3000/comics')
+    fetch(`https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${publicKey}&hash=${hash}`)
     .then((response) => response.json())
     .then((data) => {
-        for (var i = 0; i < data.length; i++) {
+
+        for (var i = 0; i < data.data.results.length; i++) {
+            let br = document.createElement('br');
 
             //comic div and elements of comic
             let newComic = document.createElement('div');
             let title = document.createElement('p');
-            let author = document.createElement('p');
-            let image = document.createElement('img')
-            image.src = data[i]['image'];
-            title.innerText = data[i]['title'];
-            author.innerText = data[i]['author'];
+            title.style.fontWeight = 'bold';
+            title.style.fontSize = '20px';
+            // let author = document.createElement('p');
+            let image = document.createElement('img');
+            let desc = document.createElement('p');
 
-            let addComicBtn = document.createElement('button');
-            addComicBtn.innerText = 'Subscribe to This Comic';
-            
+            let path = data.data.results[i]['thumbnail']['path'];
+            image.src = `${path}.jpg`
+
+            image.style.width = '100px'
+            image.style.length = '100px'
+
+            title.innerText = data.data.results[i]['title'];
+            // author.innerText = data.data.results[i]['author'];
+            desc.innerText = data.data.results[i]['description'].split('<br>')[0];
+
+            //user can subscribe to comic
+            // let addComicBtn = document.createElement('button');
+            // addComicBtn.innerText = 'Subscribe to This Comic';
+            // addComicBtn.style.color = 'red'
+
             //adding comics to div
-            newComic.addChild(image);
-            newComic.addChild(title);
-            newComic.addChild(author);
-            newComic.addChild(addComicBtn);
+            newComic.appendChild(image);
+            newComic.appendChild(title);
+            // newComic.appendChild(author);
+            newComic.appendChild(desc);
+            // newComic.appendChild(addComicBtn);
 
-            allComicsDiv.addChild(newComic);
+            allComicsDiv.appendChild(newComic);
         }
     })
 }
