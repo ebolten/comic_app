122b2cc4af75ddb515b399e3c08654946cd2cb58
Emily Bolten <emilybolten@gmail.com>
2019-07-10 09:30:38 -0400

    backend Site object

--- comic_backend/app/controllers/sites_controller.rb
+++ comic_backend/app/controllers/sites_controller.rb
@@ -1,2 +1,18 @@
 class SitesController < ApplicationController
+
+    def new
+        @site = Site.new
+    end
+
+    def create
+        @site = Site.find_or_create_by(site_params)
+        render json: @site
+    end
+
+    private
+
+    def site_params
+        params.require(:site).permit(:user_id,:comic_id)
+    end
+
 end
